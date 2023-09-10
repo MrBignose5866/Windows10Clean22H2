e22773c1b71e07d6c3671053058fa8248722d3f8
MrBignose5866 <85959880+MrBignose5866@users.noreply.github.com>
2023-09-10 14:32:01 -0400

    Update win10-cleanup.ps1

--- win10-cleanup.ps1
+++ win10-cleanup.ps1
@@ -1,105 +1,112 @@
 # Win10CleanUp
 
 # Removing AppX Packages
-Get-AppxPackage "Microsoft.3DBuilder" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.BingFinance" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.BingNews" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.BingTranslator" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.BingSports" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.BingWeather" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Getstarted" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.MicrosoftOfficeHub" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.MicrosoftSolitaireCollection" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Office.OneNote" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.People" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.SkypeApp" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Windows.Photos" | Remove-AppxPackage
-Get-AppxPackage "microsoft.windowscommunicationsapps" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.WindowsMaps" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.WindowsPhone" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.WindowsSoundRecorder" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.XboxApp" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.ZuneMusic" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.ZuneVideo" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.AppConnector" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.ConnectivityStore" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Office.Sway" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Messaging" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.CommsPhone" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.WindowsFeedbackHub" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Wallet" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.ScreenSketch" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.GetHelp" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Xbox.TCUI" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.XboxGameOverlay" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.XboxGamingOverlay" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Xbox.TCUI" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.XboxSpeechToTextOverlay" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.XboxGameCallableUI" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.MixedReality.Portal" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.MicrosoftPowerBIForWindows" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Microsoft3DViewer" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.GamingServices" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.FreshPaint" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.MicrosoftStickyNotes" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.WindowsAlarms" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.WindowsCalculator" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.WindowsCamera" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.YourPhone" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Windows.CloudExperienceHost" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Windows.ContentDeliveryManager" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Windows.PeopleExperienceHost" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.OneConnect" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.MSPaint" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.BingFoodAndDrink" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.BingHealthAndFitness" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.BingTravel" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.WindowsReadingList" | Remove-AppxPackage
-Get-AppxPackage "2FE3CB00.PicsArt-PhotoStudio" | Remove-AppxPackage
-Get-AppxPackage "46928bounde.EclipseManager" | Remove-AppxPackage
-Get-AppxPackage "4DF9E0F8.Netflix" | Remove-AppxPackage
-Get-AppxPackage "613EBCEA.PolarrPhotoEditorAcademicEdition" | Remove-AppxPackage
-Get-AppxPackage "6Wunderkinder.Wunderlist" | Remove-AppxPackage
-Get-AppxPackage "7EE7776C.LinkedInforWindows" | Remove-AppxPackage
-Get-AppxPackage "89006A2E.AutodeskSketchBook" | Remove-AppxPackage
-Get-AppxPackage "9E2F88E3.Twitter" | Remove-AppxPackage
-Get-AppxPackage "A278AB0D.DisneyMagicKingdoms" | Remove-AppxPackage
-Get-AppxPackage "A278AB0D.MarchofEmpires" | Remove-AppxPackage
-Get-AppxPackage "ActiproSoftwareLLC.562882FEEB491" | Remove-AppxPackage
-Get-AppxPackage "CAF9E577.Plex" | Remove-AppxPackage
-Get-AppxPackage "ClearChannelRadioDigital.iHeartRadio" | Remove-AppxPackage
-Get-AppxPackage "D52A8D61.FarmVille2CountryEscape" | Remove-AppxPackage
-Get-AppxPackage "D5EA27B7.Duolingo-LearnLanguagesforFree" | Remove-AppxPackage
-Get-AppxPackage "DB6EA5DB.CyberLinkMediaSuiteEssentials" | Remove-AppxPackage
-Get-AppxPackage "DolbyLaboratories.DolbyAccess" | Remove-AppxPackage
-Get-AppxPackage "Drawboard.DrawboardPDF" | Remove-AppxPackage
-Get-AppxPackage "Facebook.Facebook" | Remove-AppxPackage
-Get-AppxPackage "Fitbit.FitbitCoach" | Remove-AppxPackage
-Get-AppxPackage "Flipboard.Flipboard" | Remove-AppxPackage
-Get-AppxPackage "GAMELOFTSA.Asphalt8Airborne" | Remove-AppxPackage
-Get-AppxPackage "KeeperSecurityInc.Keeper" | Remove-AppxPackage
-Get-AppxPackage "NORDCURRENT.COOKINGFEVER" | Remove-AppxPackage
-Get-AppxPackage "PandoraMediaInc.29680B314EFC2" | Remove-AppxPackage
-Get-AppxPackage "Playtika.CaesarsSlotsFreeCasino" | Remove-AppxPackage
-Get-AppxPackage "ShazamEntertainmentLtd.Shazam" | Remove-AppxPackage
-Get-AppxPackage "SlingTVLLC.SlingTV" | Remove-AppxPackage
-Get-AppxPackage "SpotifyAB.SpotifyMusic" | Remove-AppxPackage
-Get-AppxPackage "TheNewYorkTimes.NYTCrossword" | Remove-AppxPackage
-Get-AppxPackage "ThumbmunkeysLtd.PhototasticCollage" | Remove-AppxPackage
-Get-AppxPackage "TuneIn.TuneInRadio" | Remove-AppxPackage
-Get-AppxPackage "WinZipComputing.WinZipUniversal" | Remove-AppxPackage
-Get-AppxPackage "XINGAG.XING" | Remove-AppxPackage
-Get-AppxPackage "flaregamesGmbH.RoyalRevolt2" | Remove-AppxPackage
-Get-AppxPackage "king.com.*" | Remove-AppxPackage
-Get-AppxPackage "king.com.BubbleWitch3Saga" | Remove-AppxPackage
-Get-AppxPackage "king.com.CandyCrushSaga" | Remove-AppxPackage
-Get-AppxPackage "king.com.CandyCrushSodaSaga" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Print3D" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.MinecraftUWP" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.NetworkSpeedTest" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.Windows.Cortana" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.MicrosoftEdge" | Remove-AppxPackage
-Get-AppxPackage "Microsoft.WindowsStore" | Remove-AppxPackage
+$Apps = @(
+"Microsoft.3DBuilder"
+"Microsoft.BingFinance"
+"Microsoft.BingNews" 
+"Microsoft.BingTranslator" 
+"Microsoft.BingSports" 
+"Microsoft.BingWeather" 
+"Microsoft.Getstarted" 
+"Microsoft.MicrosoftOfficeHub" 
+"Microsoft.MicrosoftSolitaireCollection" 
+"Microsoft.Office.OneNote" 
+"Microsoft.People" 
+"Microsoft.SkypeApp" 
+"Microsoft.Windows.Photos" 
+"microsoft.windowscommunicationsapps" 
+"Microsoft.WindowsMaps" 
+"Microsoft.WindowsPhone" 
+"Microsoft.WindowsSoundRecorder" 
+"Microsoft.XboxApp" 
+"Microsoft.ZuneMusic" 
+"Microsoft.ZuneVideo" 
+"Microsoft.AppConnector" 
+"Microsoft.ConnectivityStore" 
+"Microsoft.Office.Sway" 
+"Microsoft.Messaging" 
+"Microsoft.CommsPhone" 
+"Microsoft.WindowsFeedbackHub" 
+"Microsoft.Wallet" 
+"Microsoft.ScreenSketch" 
+"Microsoft.GetHelp" 
+"Microsoft.Xbox.TCUI" 
+"Microsoft.XboxGameOverlay" 
+"Microsoft.XboxGamingOverlay" 
+"Microsoft.Xbox.TCUI" 
+"Microsoft.XboxSpeechToTextOverlay" 
+"Microsoft.XboxGameCallableUI" 
+"Microsoft.MixedReality.Portal" 
+"Microsoft.MicrosoftPowerBIForWindows" 
+"Microsoft.Microsoft3DViewer" 
+"Microsoft.GamingServices" 
+"Microsoft.FreshPaint" 
+"Microsoft.MicrosoftStickyNotes" 
+"Microsoft.WindowsAlarms" 
+"Microsoft.WindowsCalculator" 
+"Microsoft.WindowsCamera" 
+"Microsoft.YourPhone" 
+"Microsoft.Windows.CloudExperienceHost" 
+"Microsoft.Windows.ContentDeliveryManager" 
+"Microsoft.Windows.PeopleExperienceHost" 
+"Microsoft.OneConnect" 
+"Microsoft.MSPaint" 
+"Microsoft.BingFoodAndDrink" 
+"Microsoft.BingHealthAndFitness" 
+"Microsoft.BingTravel" 
+"Microsoft.WindowsReadingList" 
+"2FE3CB00.PicsArt-PhotoStudio" 
+"46928bounde.EclipseManager" 
+"4DF9E0F8.Netflix" 
+"613EBCEA.PolarrPhotoEditorAcademicEdition" 
+"6Wunderkinder.Wunderlist" 
+"7EE7776C.LinkedInforWindows" 
+"89006A2E.AutodeskSketchBook" 
+"9E2F88E3.Twitter" 
+"A278AB0D.DisneyMagicKingdoms" 
+"A278AB0D.MarchofEmpires" 
+"ActiproSoftwareLLC.562882FEEB491" 
+"CAF9E577.Plex" 
+"ClearChannelRadioDigital.iHeartRadio" 
+"D52A8D61.FarmVille2CountryEscape" 
+"D5EA27B7.Duolingo-LearnLanguagesforFree" 
+"DB6EA5DB.CyberLinkMediaSuiteEssentials" 
+"DolbyLaboratories.DolbyAccess" 
+"Drawboard.DrawboardPDF" 
+"Facebook.Facebook" 
+"Fitbit.FitbitCoach" 
+"Flipboard.Flipboard" 
+"GAMELOFTSA.Asphalt8Airborne" 
+"KeeperSecurityInc.Keeper" 
+"NORDCURRENT.COOKINGFEVER" 
+"PandoraMediaInc.29680B314EFC2" 
+"Playtika.CaesarsSlotsFreeCasino" 
+"ShazamEntertainmentLtd.Shazam" 
+"SlingTVLLC.SlingTV" 
+"SpotifyAB.SpotifyMusic" 
+"TheNewYorkTimes.NYTCrossword" 
+"ThumbmunkeysLtd.PhototasticCollage" 
+"TuneIn.TuneInRadio" 
+"WinZipComputing.WinZipUniversal" 
+"XINGAG.XING" 
+"flaregamesGmbH.RoyalRevolt2" 
+"king.com.*" 
+"king.com.BubbleWitch3Saga" 
+"king.com.CandyCrushSaga" 
+"king.com.CandyCrushSodaSaga" 
+"Microsoft.Print3D" 
+"Microsoft.MinecraftUWP" 
+"Microsoft.NetworkSpeedTest" 
+"Microsoft.Windows.Cortana" 
+"Microsoft.MicrosoftEdge" 
+"Microsoft.WindowsStore"
+
+)
+
+ForEach ($App in $Apps){
+Get-AppxPackage $App | Remove-AppxPackage
+}
 
 # Hide Search Box
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Type DWord -Value 0
@@ -124,6 +131,9 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 
 # Set Taskbar To Left
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Type DWord -Value 0
+
+# Disable Action Center
+Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
 
 # Disable Windows Media Player
 Disable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer"
