4c837780a6d959768d96ff8824b43b94ef91d596
MrBignose5866 <85959880+MrBignose5866@users.noreply.github.com>
2023-09-10 19:41:50 -0400

    Create start.ps1

---
+++ start.ps1
@@ -0,0 +1,6 @@
+# This PowerShell .PS1 File Launches The Main Script So You Don't Have To Mess Around In The Sources Folder
+
+Set-Location ".\sources"
+
+# Launches Press A To Continue Menu
+& "./CleanUp-Main"
