437f56ccbe1c62263d7b7f4707da20201c156c51
MrBignose5866 <85959880+MrBignose5866@users.noreply.github.com>
2023-09-11 16:52:20 -0400

    Delete start.ps1

--- start.ps1
+++
@@ -1,6 +0,0 @@
-# This PowerShell .PS1 File Launches The Main Script So You Don't Have To Mess Around In The Sources Folder
-
-Set-Location ".\sources"
-
-# Launches Press A To Continue Menu
-& "./CleanUp-Main"
